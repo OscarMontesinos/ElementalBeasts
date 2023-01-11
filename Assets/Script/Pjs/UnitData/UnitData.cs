a0167d0d0d26bbb5cbbf36d04e3a179b8ea9d3a3
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2023-01-11 16:26:30 +0100

    merge

---
+++ Assets/Script/Pjs/UnitData/UnitData.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitData
+{
+    public string name;
+    public Sprite icon;
+    public Sprite art;
+    public int hab1Selected;
+    public int hab2Selected;
+    public int hab3Selected;
+    public int hab4Selected;
+    public int itemSelected;
+    public int hab1;
+    public string hab1Name;
+    public string hab1Description;
+    public int hab2;
+    public string hab2Name;
+    public string hab2Description;
+    public int hab3;
+    public string hab3Name;
+    public string hab3Description;
+    public int hab4;
+    public string hab4Name;
+    public string hab4Description;
+    public int hab5;
+    public string hab5Name;
+    public string hab5Description;
+    public int hab6;
+    public string hab6Name;
+    public string hab6Description;
+    public int hab7;
+    public string hab7Name;
+    public string hab7Description;
+    public int hab8;
+    public string hab8Name;
+    public string hab8Description;
